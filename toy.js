6a30f397c10406ba589faf1a824f81c1d4c31fc4
867208091@qq.com <867208091@qq.com>
2018-07-11 15:53:24 +0800

    test

--- toy.js
+++ toy.js
@@ -88,4 +88,4 @@ var right = function(){
     toy.face = directs[i];
 };
 
-//console.log("Please type in command line by line, or 'exit' to exit:")
+//console---------.log("Please type in command line by line, or 'exit' to exit:")
